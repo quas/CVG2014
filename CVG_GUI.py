946a718120a271ff97cb8737956ce642782e6542
quas <fess.root@gmail.com>
2014-05-26 07:29:01 +0500

    ui init

--- CVG_GUI.py
+++ CVG_GUI.py
@@ -1,39 +1,33 @@
-
-
 from PyQt4 import QtGui, QtCore
 import sys
 import platform
 
-OS_PLATFORM = platform.system()
+
 label = False
 app = QtGui.QApplication(sys.argv)
-window = QtGui.QWidget()
 
 class myWindow(QtGui.QWidget):
+
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setWindowTitle('CVG2014')
         self.setFixedSize(900, 550)
-        self.setWindowIcon(QtGui.QIcon('donkey.ico'))
+        self.setWindowIcon(QtGui.QIcon('Icon.ico'))
+        self.label = QtGui.QLabel('....', self)
+        self.label.setFixedWidth(300)
+        self.label.move(100, 8)
+        self.button = QtGui.QPushButton('open file', self)
+        self.button.clicked.connect(self.buttonClicked)
 
     def buttonClicked(self):
-        global OS_PLATFORM
-        global label
-        if (not label):
-            label = QtGui.QLabel('....')
-            label.move(20, 0)
-            vbox = QtGui.QVBoxLayout()
-            vbox.addWidget(label)
-            self.setLayout(vbox)
-
+        OS_PLATFORM = platform.system()
         sender = self.sender()
         directory = '/home' if (OS_PLATFORM is 'Linux') else 'E:\PythonProjects\GEO_PROJ\GEO'
-        fileName =  QtGui.QFileDialog.getOpenFileName(sender, 'Open Xml file with points', directory, 'XML *.xml')
-        label.setText(fileName)
-        label.move(20, 0)
+        fileName = QtGui.QFileDialog.getOpenFileName(sender, 'Open Xml file with points', directory, 'XML *.xml')
+        fileName = fileName[fileName.rfind('/')+1:]
+        self.label.setText(fileName)
+
 
 win = myWindow()
-button = QtGui.QPushButton('open file', win)
-button.clicked.connect(win.buttonClicked)
 win.show()
 app.exec_()
