36e150d3731c6a2c3a030f304c98445ac5c1bf4a
quas <fess.root@gmail.com>
2014-05-28 11:15:37 +0500

    path

--- CVG_GUI.py
+++ CVG_GUI.py
@@ -2,19 +2,15 @@ from PyQt4 import QtGui, QtCore
 import sys
 import platform
 
-
-
-label = False
-app = QtGui.QApplication(sys.argv)
-
 class myWindow(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setWindowTitle('CVG2014')
         self.setFixedSize(350, 350)
-        self.setWindowIcon(QtGui.QIcon('Icon.ico'))
+        self.setWindowIcon(QtGui.QIcon('static/Icon.png'))
         self.setStyleSheet("QMainWindow {background-image: url(static/background.png);}")
+        self.directory = ''
 
 
         self.labelFilename = QtGui.QLabel('Select .XML file with points', self)
@@ -22,37 +18,58 @@ class myWindow(QtGui.QMainWindow):
         self.labelFilename.setFixedHeight(25)
         self.labelFilename.move(10, 5)
         self.labelFilename.setStyleSheet("QLabel { background-color: white; \
-                                           border: 1px solid black; \
+                                           border: 1px solid grey; \
                                            color: grey;}")
 
+        self.SB_WidthOfXAxis = QtGui.QSpinBox(self)
+        self.SB_WidthOfXAxis.move(10, 35)
+        self.SB_WidthOfXAxis.setFixedWidth(50)
+        self.SB_WidthOfXAxis.setMaximum(9999)
 
-        self.spinBox = QtGui.QSpinBox(self)
-        self.spinBox.move(10, 35)
-        self.spinBox.setFixedWidth(50)
-        self.spinBox.setMaximum(9999)
         self.labelPointsWidth = QtGui.QLabel('Length of points on X axis', self)
         self.labelPointsWidth.setFixedWidth(250)
         self.labelPointsWidth.setFixedHeight(25)
         self.labelPointsWidth.move(65, 47.5)
 
+        self.SB_HeightAboveSeaLevel = QtGui.QSpinBox(self)
+        self.SB_HeightAboveSeaLevel.move(10, 75)
+        self.SB_HeightAboveSeaLevel.setFixedWidth(50)
+        self.SB_HeightAboveSeaLevel.setRange(-9999, 9999)
 
+        self.labelPointsSeaLevel = QtGui.QLabel('Height above sea level', self)
+        self.labelPointsSeaLevel.setFixedWidth(250)
+        self.labelPointsSeaLevel.setFixedHeight(25)
+        self.labelPointsSeaLevel.move(65, 87)
 
-        self.button = QtGui.QPushButton('...', self)
-        self.button.setFixedWidth(30)
-        self.button.setFixedHeight(27)
-        self.button.move(311, 4)
-        self.button.clicked.connect(self.buttonClicked)
+        self.buttonOpenFile = QtGui.QPushButton('...', self)
+        self.buttonOpenFile.setFixedWidth(30)
+        self.buttonOpenFile.setFixedHeight(27)
+        self.buttonOpenFile.move(311, 4)
+        self.buttonOpenFile.clicked.connect(self.buttonClicked)
+
+        self.buttonGetVolume = QtGui.QPushButton('RUN', self)
+        self.buttonGetVolume.setFixedWidth(52)
+        self.buttonGetVolume.setFixedHeight(35)
+        self.buttonGetVolume.move(9, 115)
+
+        self.labelGetVolume = QtGui.QLabel('Get Vol', self)
+        self.labelGetVolume.setFixedWidth(52)
+        self.labelGetVolume.setFixedHeight(32)
+        self.labelGetVolume.move(72, 117)
+        self.labelGetVolume.setStyleSheet("QLabel { background-color: white; \
+                                           border: 1px solid grey; \
+                                           color: grey;}")
 
     def buttonClicked(self):
-        OS_PLATFORM = platform.system()
+
         sender = self.sender()
-        directory = '/home' if (OS_PLATFORM is 'Linux') else 'E:\PythonProjects\GEO_PROJ\GEO'
-        fileName = QtGui.QFileDialog.getOpenFileName(sender, 'Open Xml file with points', directory, 'XML *.xml')
-        fileName = fileName[fileName.rfind('/')+1:]
+        # directory = '/home' if (OS_PLATFORM is 'Linux') else 'E:\PythonProjects\GEO_PROJ\GEO'
+        path = QtGui.QFileDialog.getOpenFileName(sender, 'Open Xml file with points', self.directory, 'XML *.xml')
+        fileName = path[path.rfind('/')+1:]
+        self.directory = path[:path.rfind('/')]
         self.labelFilename.setText(fileName)
 
-
+app = QtGui.QApplication(sys.argv)
 win = myWindow()
-
 win.show()
 app.exec_()
